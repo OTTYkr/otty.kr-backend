1eb25b5e753f1bee06d79257c19ed5400ba1ebf3
kbhetrr <kimkh7534@naver.com>
2024-01-04 14:37:56 +0900

    KKH: 스케쥴러 코드 수정 중

--- schedulers/yahoo_screener.py
+++ schedulers/yahoo_screener.py
@@ -38,7 +38,9 @@ class YahooScreener:
         service = Service(ChromeDriverManager().install())
         options = webdriver.ChromeOptions()
         # 창 숨기는 옵션 추가
-        options.add_argument("headless")
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1; WOW64; Trident/7.0; rv:11.0) like Gecko")
         driver = webdriver.Chrome(service=service, options=options)
 
